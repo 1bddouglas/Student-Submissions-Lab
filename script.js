ebc0318e2261d66a7a1ef298a5e796801ed356de
Brandon <1bddouglas@gmail.com>
2022-10-05 17:24:48 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,153 @@
+"use strict";
+
+// 1.
+// ---------------------------------------------------------------------------------------------------------------
+// submissions is an array of objects
+const submissions = [
+  {
+    name: "Jane",
+    score: 95,
+    date: "2020-01-24",
+    passed: true,
+  },
+  {
+    name: "Joe",
+    score: 77,
+    date: "2018-05-14",
+    passed: true,
+  },
+  {
+    name: "Jack",
+    score: 59,
+    date: "2020-07-05",
+    passed: false,
+  },
+  {
+    name: "Jill",
+    score: 88,
+    date: "2020-04-22",
+    passed: true,
+  },
+];
+
+// 2.
+// ---------------------------------------------------------------------------------------------------------------
+const addSubmission = (array, newName, newScore, newDate) => {
+  let newPassed = null;
+  if (newScore >= 60) {
+    newPassed = true;
+  } else {
+    newPassed = false;
+  }
+  const studentInfo = {
+    name: newName,
+    score: newScore,
+    date: newDate,
+    passed: newPassed,
+  };
+  // now we use push() method to add the new array, studentInfo, to the submissions array
+  array.push(studentInfo);
+};
+
+// not interested in addSubmission's return value,
+// ... so I don't call it in a console log
+addSubmission(submissions, "James", 100, "2022-10-05");
+addSubmission(submissions, "Andrea", 50, "2022-10-05");
+// check work: (data that were modified)
+console.log(submissions);
+// both were added to the array, and their passed status was calculated properly
+
+// 3.
+// ---------------------------------------------------------------------------------------------------------------
+const deleteSubmissionsByIndex = (array, index) => {
+  array.splice(index, 1);
+};
+deleteSubmissionsByIndex(submissions, 1);
+// check work
+console.log(submissions);
+// deleted Joe from the second place [1] in the array
+
+// 4.
+// ---------------------------------------------------------------------------------------------------------------
+const deleteSubmissionsByName = (array, name) => {
+  const foundName = array.findIndex((studentName) => {
+    return studentName.name === name;
+  });
+  array.splice(foundName, 1);
+};
+deleteSubmissionsByName(submissions, "James");
+// check work
+console.log(submissions);
+// deleted James
+
+// 5.
+// ---------------------------------------------------------------------------------------------------------------
+/* const editSubmission = (array, index, score) => {
+  let newScore = (array[index].score = score);
+  newScore.passed >= 60 ? true : false;
+};
+// check work
+console.log(editSubmission(submissions, [0], 40));
+// got undefined, unsure why. Come back later
+*/
+
+// 6.
+// ---------------------------------------------------------------------------------------------------------------
+const findSubmissionByName = (array, name) => {
+  return array.find((item) => {
+    return (item.name = name);
+  });
+};
+//check work
+console.log(findSubmissionByName(submissions, "Jane"));
+// logging for "Jane"
+
+// 7.
+// ---------------------------------------------------------------------------------------------------------------
+const findLowestScore = (array) => {
+  let lowest = 0;
+  array.forEach((item) => {
+    lowest = Math.min(item.score);
+  });
+  return lowest;
+};
+//check work
+console.log(findLowestScore(submissions));
+// returned 50, Andrea's score, as the lowest
+
+// 8.
+// ---------------------------------------------------------------------------------------------------------------
+const findAverageScore = (array) => {
+  let averageScore = 0;
+  for (const item of array) {
+    averageScore += item.score / array.length;
+  }
+  return averageScore;
+};
+// check work
+console.log(findAverageScore(submissions));
+// seems good
+
+// 9.
+// ---------------------------------------------------------------------------------------------------------------
+const filterPassing = (array) => {
+  const filteredPassed = array.filter((item) => {
+    return item.passed === true;
+  });
+  return filteredPassed;
+};
+// check work
+console.log(filterPassing(submissions));
+// returned two passing scores
+
+// 10.
+// ---------------------------------------------------------------------------------------------------------------
+const filter90AndAbove = (array) => {
+  const filtered90 = array.filter((item) => {
+    return item.score >= 90;
+  });
+  return filtered90;
+};
+// check work
+console.log(filter90AndAbove(submissions));
+// returned with Jane's score of 95, so it worked
